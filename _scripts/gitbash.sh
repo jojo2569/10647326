c761f1e27998945f83d64d1fcee18597d78d4b50
jojo2569 <joevelardi25@gmail.com>
2023-07-13 02:53:01 +0000

    Add All

---
+++ _scripts/gitbash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Push to GIT Repo
+
+
+read -p "Files/Folders:" content
+
+if [ -z "$content"]
+then
+	$content="."
+fi
+
+
+git add $content
+read -p "Commit description: " desc
+git commit -m "$desc"
+#git branch -M main
+#git push origin master
