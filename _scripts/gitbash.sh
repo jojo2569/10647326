880567a60704a8e78d043cd06a6484cf9fb38511
jojo2569 <joevelardi25@gmail.com>
2023-07-13 03:11:35 +0000

    Full Repo Upload

--- _scripts/gitbash.sh
+++ _scripts/gitbash.sh
@@ -2,6 +2,9 @@
 
 # Push to GIT Repo
 
+#GitHub Username
+uname="jojo2569"
+token="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"
 
 read -p "Files/Folders     : " content
 
@@ -14,5 +17,10 @@ fi
 git add $content
 read -p "Commit description: " desc
 git commit -m "$desc"
-#git branch -M main
-#git push origin master
+git branch -M main
+git push origin master
+
+sleep 10
+printf uname
+sleep 10
+printf token
