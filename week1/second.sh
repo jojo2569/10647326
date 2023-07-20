00da950f3c3a52b133b333c50609ee0eb662ceaa
jojo2569 <joevelardi25@gmail.com>
2023-07-20 11:52:17 +0000

    Added Commentry to Scripts

--- week1/second.sh
+++ week1/second.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Purpose      : Prints texta to the terminal, including user input.
+# Filename     : second.sh
+# Date Created : 14-Jul-23
+# Date Modified: 20-Jul-23
+# Author       : Joe Velardi
+
+
 echo "Hi there!"
 
 echo "It's good to see you $1!"
