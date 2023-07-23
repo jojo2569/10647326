cd1ca1da9a721b71b89db8066889250d598f193e
jojo2569 <joevelardi25@gmail.com>
2023-07-23 12:49:10 +0000

    Week 3 Script Updates

--- week3/calculator.sh
+++ week3/calculator.sh
@@ -16,65 +16,65 @@ NORMAL="\033[0m"
 
 while [ "$?" -eq 0 ] ; do
 
-    clear
+	clear
 
-    echo -e  "\nCalculator. Enter a Menu Option."
-    echo -e  "  1. Addition"
-    echo -e  "  2. Subtraction"
-    echo -e  "  3. Multiplication"
-    echo -e  "  4. Division"
-    echo -e  "  5. Exit"
+	echo -e  "\nCalculator. Enter a Menu Option."
+	echo -e  "  1. Addition"
+	echo -e  "  2. Subtraction"
+	echo -e  "  3. Multiplication"
+	echo -e  "  4. Division"
+	echo -e  "  5. Exit"
 
-    read -rp "Option [1-5]: " option
+	read -rp "Option [1-5]: " option
 
-    # Read Inputs if Menu  Options [1-4]
-    if [[ "$option" -ge 1 && "$option" -le 4 ]] ; then
+	# Read Inputs if Menu  Options [1-4]
+	if [[ "$option" -ge 1 && "$option" -le 4 ]] ; then
 
-        echo -e "\n"
-        read -rp "Enter First Value : " val1
-        read -rp "Enter Second Value: " val2  
+		echo -e "\n"
+		read -rp "Enter First Value : " val1
+		read -rp "Enter Second Value: " val2  
 
-    fi
+	fi
 
-    #Perform Calculation
+	#Perform Calculation
 	case $option in
 		[1]* )
-            sum=$(echo "$val1 + $val2" | bc)
+			sum=$(echo "$val1 + $val2" | bc)
 			echo -e "\n${BLUE}Answer: $val1 + $val2 = $sum\n" 
 			;;
-        [2]* )
-            sum=$(echo "$val1 - $val2" | bc)
+		[2]* )
+			sum=$(echo "$val1 - $val2" | bc)
 			echo -e "\n${GREEN}Answer: $val1 - $val2 = $sum\n" 
 			;;
-        [3]* )
-            sum=$(echo "$val1 * $val2" | bc)
+		[3]* )
+			sum=$(echo "$val1 * $val2" | bc)
 			echo -e "\n${RED}Answer: $val1 x $val2 = $sum\n" 
 			;;
-    	[4]* ) 
-            sum=$(echo "$val1 / $val2" | bc)
+		[4]* ) 
+			sum=$(echo "$val1 / $val2" | bc)
 			echo -e "\n${PURPLE}Answer: $val1 / $val2 = $sum\n" 
 			;;
-        [5]* )
-            echo -e "\nExiting ...\n"
-            exit 1
-            ;;
+		[5]* )
+			echo -e "\nExiting ...\n"
+			exit 1
+			;;
 		* )
 			echo -e "\nIncorrect Value. Exiting.\n"
-            exit 1
+			exit 1
 			;;
 	esac    
 
-    echo -e "$NORMAL"
-    read -rp "Perform Another Calculation? [yn] " yn
+	echo -e "$NORMAL"
+	read -rp "Perform Another Calculation? [yn] " yn
 
-    case $yn in
+	case $yn in
 		[Yy]* )
 			;;
 		* )
-            echo -e "\nExiting ...\n"
+			echo -e "\nExiting ...\n"
 			exit 1
 			;;
-    esac
+	esac
 
 done
 
