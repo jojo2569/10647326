4f2bef1a93c74e5c596823edfae1903064ff7147
jojo2569 <joevelardi25@gmail.com>
2023-07-24 14:03:10 +0000

    Week 2 Script Updates

--- week2/folderMaker.sh
+++ week2/folderMaker.sh
@@ -13,11 +13,11 @@ function createFolder() {
 	read -rp "Type the name of the folder you would like to create: " folderName
 
 
-	if [[ -d "$folderName" ]]
+	if [ -d "$folderName" ]
 	then
 		echo -e "\nFolder $folderName already exists.\n"
 		((cntDupe+=1))
-        else
+	else
 		mkdir "$folderName"
 		echo -e "\nFolder $folderName created\n"
 		((cntGood+=1))
