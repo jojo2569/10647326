bc731b060a943bdc7cf557f44814326d226512c8
jojo2569 <joevelardi25@gmail.com>
2023-07-30 06:26:34 +0000

    Week 3 Script Updates

--- week3/internetDownloader.sh
+++ week3/internetDownloader.sh
@@ -7,35 +7,47 @@
 # Author       : Joe Velardi
 
 
-clear
+#Constants
+RED="\033[0;31m"
+NORMAL="\033[0m"
 
 while true
 do
+
+    clear
+
     # Read URL or exit.
     read -rp "Please Type the URL of a File to Download, or Type 'exit' to quit: " url
 
-    if [ "$url" != "exit" ]
+    isExit=$(echo "$url" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$isExit" != "exit" ]
     then
 
-        #Read Location to Store File.
-        read -rp "Type the Location of Where You Would Like to Download the File to: " location
-     
-        mkdir -p "$location"                            	#Creates Folder; Prevents Error on Existing Folder.        
-        wget -qP "$location" "$url"     			#Gets File and Copies to the Above Location.
+        #Test for Valid URL.
+        wget -q --spider "$url"
 
-	#Test for Valid URL/File Location
-	if [ "$?" -eq 8 ]
-	then
-		echo -e "\nInvalid URL. Try again ...\n"
-	else
+        #Valid URL/File Location
+        if [ "$?" -eq 0 ]
+        then
 
+            #Read Location to Store File.
+            read -rp "Type the Location of Where You Would Like to Download the File to: " location
 
-		doc=$(echo -e "$url" | awk -F"/" '{print $NF}') #Get Document name from URL.
-        
-		#Display Output of Downloaded Content.
-		echo -e "\nFile Location" && ls -al "$location"/"$doc" --color=auto && echo -e "\n"
-		sleep 1
-	fi
+            mkdir -p "$location"                                #Creates Folder; Prevents Error on Existing Folder.        
+            wget -qP "$location" "$url"     			        #Gets File and Copies to the Above Location.
+
+            doc=$(echo -e "$url" | awk -F"/" '{print $NF}')     #Get Document name from URL.
+            
+            #Display Output of Downloaded Content.
+            echo -e "\nFile Location" && ls -al "$location"/"$doc" --color=auto && echo -e "\n"
+            
+            read -rp "Press Any Key to Continue"
+
+        else
+            echo -e "\n${RED}Invalid URL. Try again ...${NORMAL}\n"
+            sleep 2
+        fi
 
     else
 
