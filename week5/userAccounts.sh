152529b0fb665447534c01d8c6f58cdb2a609457
jojo2569 <joevelardi25@gmail.com>
2023-08-02 15:33:12 +0800

    Week 5 Script Updates

--- week5/userAccounts.sh
+++ week5/userAccounts.sh
@@ -7,38 +7,26 @@
 # Author       : Joe Velardi
 
 
-#Constants
-BLACK="\033[30m"
-RED="\033[31m"
-GREEN="\033[32m"
-BROWN="\033[033m"
-BLUE="\033[34m"
-PURPLE="\033[35m"
-CYAN="\033[36m"
-GREY="\033[37m"
-NORMAL="\033[0m"
-BG_RED="\033[101m"
-BG_GREEN="\033[102m"
-BG_WHITE="\033[107m"
+clear
 
-#Variables
-passwdList=$(cat /etc/passwd)
+grep '/bin/bash' /etc/passwd | awk '
 
-#echo -e $passwdList
-
-grep '/bin/bash' /etc/passwd | awk 'BEGIN {
+BEGIN {
     FS=":";
     BLUE="\033[34m";
+    BROWN="\033[033m";
+    PURPLE="\033[35m";
+    NORMAL="\033[0m";
 
-    print "__________________________________________________________________________________________________________";
-    print "| Username             | UserID   | GroupID  | Home                           | Shell                     |";
-
+    print "________________________________________________________________________________________________";
+    printf("| %sUsername%s   | %sUserID%s     | %sGroupID%s    | %sHome%s                           | %sShell%s                |\n", BLUE, NORMAL, BLUE, NORMAL, BLUE, NORMAL, BLUE, NORMAL, BLUE, NORMAL);
+    print "________________________________________________________________________________________________";
 }
 
 {
-    printf("| \033[33m%-20s\033[0m | \033[35m%-8s\033[0m | \033[35m%-8s\033[0m | \033[35m%-30s\033[0m | \033[35m%-25s\033[0m |\n", $1, $3, $4, $6, $7);
+    printf("| %s%-10s%s | %s%-10s%s | %s%-10s%s | %s%-30s%s | %s%-20s%s |\n", BROWN, $1, NORMAL, PURPLE, $3, NORMAL, PURPLE, $4, NORMAL, PURPLE, $6, NORMAL, PURPLE, $7, NORMAL);
 }
 
 END {
-    print "__________________________________________________________________________________________________________";
+    printf ("________________________________________________________________________________________________\n\n");
 }' 
