b89352dbdc4680e0e2c0d4765c9af4976c612214
Joe <joevelardi25@gmail.com>
2023-08-12 11:04:16 +0800

    Assignment #4 Script Updates

---
+++ assignment/scripts/downloadData.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Purpose      : Download Web Data.
+# Filename     : downloadData.sh
+# Date Created : 12-Aug-2023
+# Date Modified: 12-Aug-2023
+# Author       : Joe Velardi
+
+
+#Variables
+url="https://haveibeenpwned.com/PwnedWebsites"
+location="../files"
+
+#Test for Valid URL.
+wget -q --spider "$url"
+
+#Valid URL/File Location
+if [ "$?" -eq 0 ]
+then
+     
+    wget -qP "$location" "$url"     			        #Gets File and Copies to the Above Location.
+
+    doc=$(echo -e "$url" | awk -F"/" '{print $NF}')     #Get Document name from URL.
+    
+    #Display Output of Downloaded Content.
+    echo -e "\nFile Downloaded Successfully ..." && ls -al "$location"/"$doc" --color=auto && echo -e "\n"
+    sleep 2
+
+else
+    echo -e "\n${RED}Invalid URL ...${NORMAL}\n"
+    sleep 2
+fi
