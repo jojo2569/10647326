d4e8cb1af6445b07e7bde4c0f8344564347f39f7
Joe <joevelardi25@gmail.com>
2023-08-12 09:59:31 +0800

    Assessment #4 Script Updates

---
+++ assignment/main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Purpose      : Assessment #4
+# Filename     : main.sh
+# Date Created : 12-Aug-2023
+# Date Modified: 12-Aug-2023
+# Author       : Joe Velardi
+
+
+#References
+
+
+#Constants
+RED="\033[31m"
+GREEN="\033[32m"
+BROWN="\033[033m"
+BLUE="\033[34m"
+PURPLE="\033[35m"
+CYAN="\033[36m"
+GREY="\033[37m"
+NORMAL="\033[0m"
+
+
+#Variables
+
+
+#Main
