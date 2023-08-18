06ed052ec5e039a1148d7cb71bfac1543d32e599
Joe <joevelardi25@gmail.com>
2023-08-18 22:26:43 +0800

    Cleaning up Scripts

--- assignment/scripts/downloadData.sh
+++ assignment/scripts/downloadData.sh
@@ -28,24 +28,24 @@ then
     displayBanner 
      
     #Gets File and Makes a Local Copy.
-    echo -e "\n${GREEN}  Downloading Data. One Moment ...${NORMAL}"
+    echo -e "\n${BGREEN}  Downloading Data. One Moment ...${NORMAL}"
     wget -qP "$dataLocation" "$url"
 
     #Displays Content Being Processes.
     results=$(ls -al "$dataSource")
-    echo -e "\n  File Downloaded Successfully ...\n  $results"
+    echo -e "\n${GREEN}  File Downloaded Successfully ...${NORMAL}\n  $results"
 
     #Run Data Cleansing.
-    echo -e "\n${GREEN}  Cleansing Data. Almost There ...${NORMAL}"
+    echo -e "\n${BGREEN}  Cleansing Data. Almost There ...${NORMAL}"
     $(./cleanseData.sh)
     
     entries=$(wc -l $dataCleansed | grep -o "^\w*\b")
     results=$(ls -al "$dataCleansed")
-    echo -e "\n  Data Cleansing Completed ($entries Entries) ...\n  $results"
+    echo -e "\n${GREEN}  Data Cleansing Completed ($entries Entries) ...${NORMAL}\n  $results"
 
     entries=$(wc -l $dataCategory | grep -o "^\w*\b")
     results=$(ls -al "$dataCategory")
-    echo -e "\n  Data Category Completed ($entries Entries) ...\n  $results\n"
+    echo -e "\n${GREEN}  Data Category Completed ($entries Entries) ...${NORMAL}\n  $results\n"
 
     promptPressEnter
 
